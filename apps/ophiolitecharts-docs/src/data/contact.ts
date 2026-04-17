4fbecb41734e342f55641c906b015886344cf286
tuna-soup <stefanus.crooijmans@gmail.com>
2026-04-17 16:13:14 +0200

    Sharpen charts site pricing and contact flow

---
+++ apps/ophiolitecharts-docs/src/data/contact.ts
@@ -0,0 +1,7 @@
+export const evaluationEmail = "stefanus.crooijmans@gmail.com";
+
+export const evaluationMailto = `mailto:${evaluationEmail}?subject=Ophiolite%20Charts%20Evaluation&body=Company%3A%0AUse%20case%3A%0AChart%20families%3A%0AWrapper%20surface%20(JS%20or%20Svelte)%3A%0ATimeline%3A%0AQuestions%3A%0A`;
+
+export const teamMailto = `mailto:${evaluationEmail}?subject=Ophiolite%20Charts%20Team%20License&body=Company%3A%0AProduct%3A%0AExpected%20users%3A%0ARequired%20chart%20families%3A%0ASupport%20needs%3A%0A`;
+
+export const enterpriseMailto = `mailto:${evaluationEmail}?subject=Ophiolite%20Charts%20Enterprise%20Rollout&body=Company%3A%0AProgram%20scope%3A%0ARequired%20chart%20families%3A%0AIntegration%20surface%3A%0APerformance%20or%20support%20requirements%3A%0A`;
